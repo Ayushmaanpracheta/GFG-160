cd42b6c74b5fa5aec8b55a7e03759c65c7caa0bd
Ayushmaan Gautam <ayushmaangautam101@gmail.com>
2025-03-29 23:33:03 +0530

    Add files via upload

---
+++ GFG-160 days/Array - 13days/Kadane's Algorithm.cpp
@@ -0,0 +1,74 @@
+// Kadane's Algorithm
+// Question:
+/*
+Given an integer array arr[]. You need to find the maximum sum of a subarray.
+
+Examples:
+
+Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output: 11
+Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+Input: arr[] = [-2, -4]
+Output: -2
+Explanation: The subarray {-2} has the largest sum -2.
+Input: arr[] = [5, 4, 1, 7, 8]
+Output: 25
+Explanation: The subarray {5, 4, 1, 7, 8} has the largest sum 25.
+*/
+
+// Approach 1: By iterating over all subarrays - Time Complexity-O(n^2) and Space Complexity-O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find the sum of subarray with maximum sum
+int maxSubarraySum(vector<int> &arr) {
+    int res = arr[0];
+  
+    // Outer loop for starting point of subarray
+  	for(int i = 0; i < arr.size(); i++) {
+    	int currSum = 0;
+      
+        // Inner loop for ending point of subarray
+        for(int j = i; j < arr.size(); j++) {
+        	currSum = currSum + arr[j];
+          
+            // Update res if currSum is greater than res
+            res = max(res, currSum);
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<int> arr = {2, 3, -8, 7, -1, 2, 3};
+    cout << maxSubarraySum(arr);
+    return 0;
+}
+
+// Approach 2: Using Kadane's Algorithm - Time Complexity O(n) and Space Complexity O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find the maximum subarray sum
+int maxSubarraySum(vector<int> &arr) {
+    int res = arr[0];
+    int maxEnding = arr[0];
+
+    for (int i = 1; i < arr.size(); i++) {
+      
+        // Find the maximum sum ending at index i by either extending  the maximum sum subarray ending at index i - 1 or by starting a new subarray from index i
+        maxEnding = max(maxEnding + arr[i], arr[i]);
+      
+        // Update res if maximum subarray sum ending at index i > res
+        res = max(res, maxEnding);
+    }
+    return res;
+}
+
+int main() {
+    vector<int> arr = {2, 3, -8, 7, -1, 2, 3};
+    cout << maxSubarraySum(arr);
+    return 0;
+}
